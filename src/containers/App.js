c3aafe717de46ea469d00b3a21a29f092ab848cb
hyunji-bang <groovehyunji@gmail.com>
2017-08-01 12:59:49 +0900

    Counter component structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import '../components/Counter.css'
+import Counter from '../components/Counter'
 
 class App extends Component {
     render() {
         return (
             <div>
-                Counter
+                <Counter/>
             </div>
         );
     }
