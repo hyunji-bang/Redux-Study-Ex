80540c4c901260f7304ab953c569570ac10de856
hyunji-bang <groovehyunji@gmail.com>
2017-08-03 15:00:37 +0900

    createStore to App

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import { FilterList } from './FilterList';
 
+import { createStore } from 'redux';
+import reducers from '../reducers';
+import { Provider } from 'react-redux';
+
+const store = createStore(reducers);
+
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <h1>Filter words</h1>
-                <p className="InputField">
-                    <input type="text"/>
-                </p>
-                <FilterList/>
-            </div>
+            <Provider store={store}>
+                <div className="App">
+                    <h1>Filter words</h1>
+                    <p className="InputField">
+                        <input type="text"/>
+                    </p>
+                    <FilterList/>
+                </div>
+            </Provider>
         );
     }
 }
