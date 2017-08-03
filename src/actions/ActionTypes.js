892eb244111f94de3988e9cb12d305a349053331
hyunji-bang <groovehyunji@gmail.com>
2017-08-03 14:40:13 +0900

    Define ActionTypes, ActionCreator

--- src/actions/ActionTypes.js
+++ src/actions/ActionTypes.js
@@ -1,3 +1,1 @@
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
-export const SET_COLOR = 'SET_COLOR';
+export const SET_FILTER = 'SET_FILTER';
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,6 @@
 import * as types from './ActionTypes';
 
-export const increment = () => ({
-    type: types.INCREMENT
-});
-
-export const decrement = () => ({
-    type: types.DECREMENT
-});
-
-export const setColor = (color) => ({
-    type: types.SET_COLOR,
-    color
+export const SET_FILTER = (input) => ({
+    type: types.SET_FILTER,
+    input
 });
