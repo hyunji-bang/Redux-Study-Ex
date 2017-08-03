fa2c0f75388e00600c72226b3a6bbe2d9d5c5120
hyunji-bang <groovehyunji@gmail.com>
2017-08-03 14:36:53 +0900

    Filter component in FilterList

--- src/containers/FilterList.js
+++ src/containers/FilterList.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 
+const Frameworks = ['React', 'Vue', 'Angular'];
+
+const Filter = () => {
+    return (
+        <ul>
+            {Frameworks.map((item, index) => <li>{item}</li> )}
+        </ul>
+    )
+}
+
 export class FilterList extends Component {
     render() {
-        const Frameworks = [ 'React', 'Vue', 'Angular', ];
         return (
             <div className="FilterList">
                 <h2>Filter List</h2>
-                <ul>
-                    { Frameworks.map( ( item, index, array ) => <li>{item}</li> ) }
-                </ul>
+                    {/*{ Frameworks.map( ( item, index, array ) => <li>{item}</li> ) }*/}
+                    <Filter/>
             </div>
         )
     }
