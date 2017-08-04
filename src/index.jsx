9d38a0e6800d12b5b25d741c068d18bfee31580b
hyunji-bang <groovehyunji@gmail.com>
2017-08-04 11:33:33 +0900

    Init with typescript setting

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+var ReactDOM = require("react-dom");
+var app_1 = require("./containers/app");
+require("./index.css");
+ReactDOM.render(<app_1.default />, document.getElementById('root'));
